c5c58d5d07bb50041134d5bd81a324cbb5b492ec
jero020 <102270277+jero020@users.noreply.github.com>
2026-04-28 08:21:08 -0500

    Add initial implementation of FailureFunction for string processing

---
+++ FirstTask.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <string>  
+
+
+void FailureFunction(std::string x);
+
+int main(){
+    FailureFunction("ababaa");
+    return 0;
+}
+
+void FailureFunction(std::string keyword) {
+    int n = keyword.size();
+    int f[n+1];  
+    
+    int t = 0;
+    f[1] = 0;  
+    
+    for (int s = 1; s < n; s++) {  // línea 3
+        // aquí va el while de la línea 4
+        while (t > 0 && keyword[s] != keyword[t]){
+            t = f[t];
+        }
+        
+        if (keyword[s] == keyword[t]){
+            t = t + 1;
+            f[s+1] = t;
+        }else{
+            f[s+1] = 0;
+            t = 0;
+        }
+    }
+    for (int i = 1; i <= n; i++) {
+        std::cout << f[i] << " ";
+    }
+    std::cout << std::endl;
+}
